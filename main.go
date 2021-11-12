61bee34dc142d2a32f589f37185c8e6af06116ac
Kouki Sawada <github@sawada.pro>
2021-11-13 01:33:06 +0900

    指定時間に音が鳴らせるようになった

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 )
 
 func main() {
@@ -10,12 +16,32 @@ func main() {
 	var nowTime string
 	fmt.Println("Enter time (HH:mm:ss)")
 	fmt.Scanf("%s", &SetTime)
-	fmt.Println(SetTime)
 
 	for {
 		nowTime = time.Now().Format("15:04:05")
 		if nowTime == SetTime {
 			fmt.Println("It's time !!!")
+
+			// 以下、 https://qiita.com/usk81/items/8590172a23bb71e21329 より引用
+			// ファイル名だけ変えた
+			f, err := os.Open("sound.mp3")
+			if err != nil {
+				log.Fatal(err)
+			}
+			st, format, err := mp3.Decode(f)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer st.Close()
+
+			speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+			done := make(chan bool)
+			speaker.Play(beep.Seq(st, beep.Callback(func() {
+				done <- true
+			})))
+			<-done
+
 			return
 		}
 
